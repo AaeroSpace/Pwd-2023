17ef870bfe4a390f2e1a0f7b9b14ed21fec64204
AaeroSpace <warrenaaron02@gmail.com>
2023-01-24 22:20:54 -0600

    Homework 1

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 400);
+  colorMode(HSB);
+}
+
+function draw() {
+  background(120,100,100);
+
+  circle(190, 190 ,320)
+  square(400, 25, 320)
+}
---
+++ sketch2.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+  noStroke();
+}
+
+function draw() {
+  background(255,255,255,50);
+
+  fill(255,0,0,50);
+  circle(300,180,300);
+  
+  fill(0,255,0,50);
+  circle(420,350,300);
+  
+  fill(0,0,255,50);
+  circle(190,350,300);
+}
